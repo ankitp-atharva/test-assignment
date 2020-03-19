8cf0a59826d3e81ea0e3ad3a8076366fe940b082
ankit_p <ankit.parekh@atharvasystem.com>
2020-03-19 11:30:02 +0530

    Add notice/alert message to follow action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,9 @@ class HomeController < ApplicationController
     follower = current_user.follow_users.find_or_initialize_by(follower_id: params[:user_id])
     if follower.new_record?
       follower.save
+      flash[:notice] = "You are now follows - #{follower.user.email} (User:#{follower.user_id})"
     else
-      flash[:notice] = 'Already following'
+      flash[:alert] = 'Already following'
     end
     redirect_to question_path(params[:question_id])
   end
@@ -20,8 +21,9 @@ class HomeController < ApplicationController
     follower = current_user.follow_topics.find_or_initialize_by(topic_id: params[:topic_id])
     if follower.new_record?
       follower.save
+      flash[:notice] = "You are now follows - #{follower.topic.desc} (Topic:#{follower.topic_id})"
     else
-      flash[:notice] = 'Already following'
+      flash[:alert] = 'Already following'
     end
     redirect_to question_path(params[:question_id])
   end
